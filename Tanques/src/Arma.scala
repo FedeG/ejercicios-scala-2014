f8b7ab4329026bc6cf54176a8346f225f15b2806
federicog <federicog@localhost.localdomain>
2014-03-09 17:55:48 -0300

    Agrego cantidad de usos para las armas

--- Tanques/src/Arma.scala
+++ Tanques/src/Arma.scala
@@ -1,10 +1,16 @@
-class Arma (d: Int, n: String, act: (Tanque, Tanque) => Boolean){
+class Arma (d: Int, n: String, u: Int, act: (Tanque, Tanque) => Boolean){
 	var danio: Int = d
 	var nombre: String = n
+	var usos: Int = u
 	var accion_previa_al_ataque: (Tanque,Tanque) => Boolean = act
 	
+	def puede_usarse(emisor: Tanque, receptor: Tanque): Boolean = this.usos>0 && this.accion_previa_al_ataque(emisor,receptor)
+	
 	def ataca_a(emisor: Tanque, receptor: Tanque){
-		if (accion_previa_al_ataque(emisor,receptor)) receptor.daniate(this.danio) 
+		if (this.puede_usarse(emisor,receptor)) {
+			receptor.daniate(this.danio)
+			this.usos -= 1
+		}
 	}
 	
 }
--- Tanques/src/workspace.scala
+++ Tanques/src/workspace.scala
@@ -1,8 +1,8 @@
 object workspace {
 	def main(args: Array[String]){
-		val arma = new Arma(10,"Metra",(emisor: Tanque, receptor: Tanque) => receptor.blindaje > emisor.blindaje)
-		val arma2 = new Arma(40,"XXX",(_, receptor: Tanque) => receptor.nombre == ":D")
-		val arma3 = new Arma(1,"Agua",(emisor: Tanque, _) => emisor.nombre == "Uptimus Prime")
+		val arma = new Arma(10,"Metra",10,(emisor: Tanque, receptor: Tanque) => receptor.blindaje > emisor.blindaje)
+		val arma2 = new Arma(40,"XXX",3,(_, receptor: Tanque) => receptor.nombre == ":D")
+		val arma3 = new Arma(1,"Agua",1,(emisor: Tanque, _) => emisor.nombre == "Uptimus Prime")
 		val vector_armas = Array(arma,arma2)
 		val tanque1 = new Tanque(vector_armas,100,":D")
 		val uptimus_prime = new Tanque(vector_armas:+arma3,21,"Uptimus Prime")
