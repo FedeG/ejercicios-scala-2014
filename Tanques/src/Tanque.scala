7efbd9fceebc8c04e427cddd0b5731a05dd8d765
federicog <federicog@localhost.localdomain>
2014-03-09 18:37:44 -0300

    Agrego robo de armas al matar a alguien, ver si tiene el arma, ...

--- Tanques/src/Tanque.scala
+++ Tanques/src/Tanque.scala
@@ -6,9 +6,27 @@ class Tanque (as: Array[Arma], b: Int, n: String) {
 	
 	def atacar_a(tanque_a_atacar: Tanque, nombre_arma_usar: String) {
 	  	if (this.vivo) {
-	  		var arma_a_usar = this.buscar_arma_a_usar(nombre_arma_usar)
-	  		arma_a_usar.ataca_a(this,tanque_a_atacar)
+	  		if (this.tenes_el_arma(nombre_arma_usar)) {
+	  			var arma_a_usar = this.buscar_arma_a_usar(nombre_arma_usar)
+	  			if (this.asesinaste_a_con_el_arma(tanque_a_atacar,arma_a_usar)) this.robar_armas(tanque_a_atacar)
+	  			arma_a_usar.ataca_a(this,tanque_a_atacar)
+	  		}
 	  	}
+	}
+	
+	def asesinaste_a_con_el_arma(tanque_a_atacar: Tanque, arma_a_usar: Arma): Boolean = {
+		var lo_mato = false
+		if (arma_a_usar.puede_usarse(this,tanque_a_atacar)) lo_mato = (tanque_a_atacar.blindaje < arma_a_usar.danio)
+		return lo_mato
+	}
+	
+	def robar_armas(tanque_a_robar_armas: Tanque){
+		for ( arma <- tanque_a_robar_armas.armas) {
+			if (this.tenes_el_arma(arma.nombre)) 
+			  this.buscar_arma_a_usar(arma.nombre).usos += arma.usos
+			else
+			  this.armas = this.armas :+ arma 
+		}
 	}
 	
 	def daniate(danio: Int){
@@ -20,8 +38,14 @@ class Tanque (as: Array[Arma], b: Int, n: String) {
 	
 	def morite(){
 		this.blindaje = 0
+		this.armas = Array()
 		this.vivo = false
 		println("Me mori :( (soy el tanque "+this.nombre+")")
+	}
+	
+	def tenes_el_arma(nombre_arma: String): Boolean = {
+		var index_de_arma = armas.indexWhere((arma: Arma) => arma.nombre == nombre_arma)
+		return index_de_arma != -1
 	}
 	
 	def buscar_arma_a_usar(nombre_arma_usar: String): Arma = {
--- Tanques/src/workspace.scala
+++ Tanques/src/workspace.scala
@@ -1,11 +1,11 @@
 object workspace {
 	def main(args: Array[String]){
-		val arma = new Arma(10,"Metra",10,(emisor: Tanque, receptor: Tanque) => receptor.blindaje > emisor.blindaje)
-		val arma2 = new Arma(40,"XXX",3,(_, receptor: Tanque) => receptor.nombre == ":D")
-		val arma3 = new Arma(1,"Agua",1,(emisor: Tanque, _) => emisor.nombre == "Uptimus Prime")
-		val vector_armas = Array(arma,arma2)
+		val arma_metra = new Arma(10,"Metra",10,(emisor: Tanque, receptor: Tanque) => receptor.blindaje > emisor.blindaje)
+		val arma_xxx = new Arma(40,"XXX",3,(_, receptor: Tanque) => receptor.nombre == ":D")
+		val arma_agua = new Arma(1,"Agua",1,(emisor: Tanque, _) => emisor.nombre == "Uptimus Prime")
+		val vector_armas = Array(arma_metra,arma_xxx)
 		val tanque1 = new Tanque(vector_armas,100,":D")
-		val uptimus_prime = new Tanque(vector_armas:+arma3,21,"Uptimus Prime")
+		val uptimus_prime = new Tanque(vector_armas:+arma_agua,21,"Uptimus Prime")
 		
 		tanque1.atacar_a(uptimus_prime, "Metra")
 		uptimus_prime.atacar_a(tanque1, "XXX")
@@ -21,6 +21,14 @@ object workspace {
 		uptimus_prime.atacar_a(tanque1,"XXX")
 		println("Tanque1 blindaje: " + tanque1.blindaje)
 		println("Uptimus blindaje: " + uptimus_prime.blindaje)
+		println("-----Armas de Uptimus-----")
+		for ( arma <- uptimus_prime.armas) {
+			println(arma.nombre + " usos: " + arma.usos)
+		}
+		println("-----Armas de tanque1-----")
+		for ( arma <- tanque1.armas) {
+			println(arma.nombre + " usos: " + arma.usos)
+		}
 		
 	}
 
