221a86ec2a24eaa39ae10c54d0e7fbd882115691
EzequielAyzenberg <ezequiel_ay@hotmail.com>
2014-03-10 12:00:27 -0300

    probando con los pokemones :3

---
+++ Pokemon/src/Clases.scala
@@ -0,0 +1,8 @@
+class Pokemon(n:String) {
+	var nombre : String = n
+	
+	def deciTuNombre(): String = {
+	  println(nombre)
+	  return nombre
+	}
+}
---
+++ Pokemon/src/workspace.scala
@@ -0,0 +1,8 @@
+object workspace {
+	def main(args: Array[String]){
+	  
+	  val pikachu = new Pokemon("Pikachu")
+	  
+	  pikachu.deciTuNombre
+	}
+}
