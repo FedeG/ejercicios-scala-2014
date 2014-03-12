a1c25171e8ad90ab8eb17f55f6b4a4a2317e6712
EzequielAyzenberg <ezequiel_ay@hotmail.com>
2014-03-12 16:40:48 -0300

    Modifico contenido y agrego implementacion de listas, orden superior, composicion y funcion lambda

--- LasGolondrinas/src/Golondrina.scala
+++ LasGolondrinas/src/Golondrina.scala
@@ -1,16 +1,17 @@
-class Golondrina() {
-	var salud: Int = 0
-	var ciudad: String = "Capital"
-	var nombre: String = "Golondrina Aleatoria"
+class Golondrina(s: Int = 0, c: String = "Capital", n: String = "Golondrina Aleatoria") {
+	var salud: Int = s
+	var ciudad: String = c
+	var nombre: String = n
 	
-	def tuSaludEs(unNumero:Int){
-	  salud = unNumero
-	}
+	/* Sobrecarga de métodos: Un método sobrecargado
+	 * no puede tener el mismo nombre que un atributo,
+	 * trust me
+	 */
 	
-	def dameTuSalud(): Int = salud
-	
-	def dameTuSaludEImprimila(): Int = {
-	  println(salud)
-	  return salud
-	}
+	def health(): Int = this.salud
+	def health(unNumero:Int){salud = unNumero}
+	def name(): String = (this.nombre)
+	def name(name: String){nombre = name}
+	def city(): String = this.ciudad
+	def city(unaCiudad: String){this.ciudad = unaCiudad}
 }
--- LasGolondrinas/src/workspace.scala
+++ LasGolondrinas/src/workspace.scala
@@ -1,8 +1,35 @@
 object workspace {
 	def main(args: Array[String]){
-	  val pepita = new Golondrina
-	  pepita.dameTuSaludEImprimila
-	  pepita.tuSaludEs(70)
-	  pepita.dameTuSaludEImprimila
+	  val pepita = new Golondrina(s=100,n="Pepita")
+	  val pepona = new Golondrina(s=50,n="Pepona")
+	  
+	  //Crear e imprimir una lista de numeros
+	  val listaNumeros = List(1,2,3)
+	  println(listaNumeros)
+	  
+	  //Imprimir su sumatoria
+	  println(listaNumeros.sum)
+	  
+	  //Crear e imprimir lista de Golondrinas
+	  val golondrinas = List(pepita,pepona)
+	  println(golondrinas)
+	  
+	  //Imprimir lista de vidas mappeando golondrinas
+	  //Ojo, hay una funcion lambda ayudando :3
+	  //y tambien hay inferencia de tipos
+	  val vidas = golondrinas.map(g => g.salud)
+	  println(vidas)
+	  
+	  //Imprimir lista de vidas que superen 60 
+	  //mappeando y fitrando golondrinas
+	  println(vidas.filter(vida => vida > 60))
+	  
+	  //Hacer todo la anterior de un saque!!
+	  //Composición de métodos de Scala :3
+	  println(golondrinas.map(g => g.salud).filter(vida => vida > 60))
+	  
+	  //Armar oraciones a partir 
+	  //de la lista golondrinas con foreach
+	  golondrinas.foreach(g => println("Hola! Yo soy la golondrina "+g.nombre+" y mi salud es "+g.salud))
 	}
 }
